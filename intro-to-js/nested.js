9817aaacd60e6f92c73872fc40bde265a2dc81df
fayjullah haque <fayjullah.h.emon@gmail.com>
2026-07-15 22:49:50 +0600

    ternary.js completed

---
+++ intro-to-js/nested.js
@@ -0,0 +1,14 @@
+const money = 2000;
+if(money > 500){
+    console.log('mama tui to borolok. amare khawa');
+    if(money>1000){
+        console.log('tui ki tuition er beton paisos?');
+        if(money > 5000){
+            console.log('tui t amar apon bhai');
+        }
+        
+    }
+}
+else{
+    console.log('amar kno borolok frnd nai :(');
+}
