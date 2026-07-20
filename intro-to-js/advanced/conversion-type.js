acee6752bf977a1c15f80fbbb3548ff094e12212
fayjullah haque <fayjullah.h.emon@gmail.com>
2026-07-20 07:17:38 +0600

    variable

--- intro-to-js/advanced/conversion-type.js
+++ intro-to-js/advanced/conversion-type.js
@@ -15,3 +15,23 @@ console.log(Number(null)); //0
 console.log(Number(undefined)); //NaN
 console.log(Number(true)); //1
 console.log(Number(false)); //0
+
+console.log("-----------------");
+
+console.log(Boolean("true"));
+console.log(Boolean(1));
+console.log(Boolean("Hello world"));
+console.log(Boolean("!"));
+console.log(Boolean("0"));
+console.log(Boolean("false"));
+
+console.log(Boolean(0));
+console.log(Boolean(""));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log("==================");
+let d = 0.1;
+let e = 0.2;
+let f = d+e;
+
+console.log(f==0.3);
