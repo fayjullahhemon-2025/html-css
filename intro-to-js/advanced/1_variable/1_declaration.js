ba24ab1d1bfa1255518ebe2b6c15c7ac2a684f42
fayjullah haque <fayjullah.h.emon@gmail.com>
2026-07-20 08:42:42 +0600

    intro-to-js->advanced->1_variable->1_declaration.js

---
+++ intro-to-js/advanced/1_variable/1_declaration.js
@@ -0,0 +1,20 @@
+//let const var
+//1.let
+let a = 4;
+//let a = 7; // syntax error
+// console.log(a);
+//const
+//const a = 3; //syntax error
+const b = 3;
+// console.log(b);
+
+const c = 3.1416;
+console.log(c);
+//const c = 1.675;//syntaxError
+console.log(c);
+//var
+
+var d = 5;
+// console.log(d);
+var d = 7; //redeclaration possible, which is not good practice;
+console.log(d);
