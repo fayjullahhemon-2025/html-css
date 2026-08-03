87fe128eacd1cdcc2ca4077b32dc77858c05e939
fayjullah haque <fayjullah.h.emon@gmail.com>
2026-08-03 08:02:26 +0600

    lecture 17-3 hosting.js

---
+++ module-17/lecture 17-3/4.hoisting.js
@@ -0,0 +1,12 @@
+
+console.log(add(2,3));
+function add(a,b){
+    return a+b;
+}
+//function always body soho hoisted hoy tai declare er upore function call kora jay.
+
+console.log(multiply(1,2));
+
+function multiply(a,b){
+    return a*b;
+}
