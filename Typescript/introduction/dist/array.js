4d91cd5e9bda35d99df99e4cc988611aa321639d
fayjullah haque <fayjullah.h.emon@gmail.com>
2026-08-17 16:56:08 +0600

    array

---
+++ Typescript/introduction/dist/array.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//dataType[]
+let marks = [90, 80, 70, 60];
+console.log(marks);
+//Array<Type>
+let user = [
+    "Emon",
+    "Payel",
+    "Moon"
+];
+console.log(user);
+let statusCode = [true, false, true];
+//multitype array
+let data = [
+    "Emon", 34
+];
+console.log(data);
