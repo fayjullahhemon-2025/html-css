e6912970f3e3ad67ca49b4fcb8803344115fa764
fayjullah haque <fayjullah.h.emon@gmail.com>
2026-08-18 17:51:21 +0600

    interface

--- Typescript/introduction/dist/interface.js
+++ Typescript/introduction/dist/interface.js
@@ -9,3 +9,8 @@ let employee2 = {
     id: "201",
     userName: "Emon",
 };
+;
+const multiple = (x, y) => x * y;
+console.log(multiple(2, 3));
+const addition = (...number) => number.reduce((acc, currentVal) => acc + currentVal, 0);
+console.log(addition(3, 2, 5));
