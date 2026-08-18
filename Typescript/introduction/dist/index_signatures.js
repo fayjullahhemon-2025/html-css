6e630deba3eb3c66595b5011053e636468c3099e
fayjullah haque <fayjullah.h.emon@gmail.com>
2026-08-18 20:45:38 +0600

    index signaturing, key signaturing

---
+++ Typescript/introduction/dist/index_signatures.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let languages = {
+    en: "English",
+    bn: "Bangla",
+    hi: "Hindi",
+    ur: "Urdu"
+};
+let char = {
+    1: 'a',
+    2: 'b',
+    3: 'c'
+};
+console.log(char);
