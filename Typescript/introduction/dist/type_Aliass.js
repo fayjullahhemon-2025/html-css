70307569419656886cc5282f710dbe861cb8b88f
fayjullah haque <fayjullah.h.emon@gmail.com>
2026-08-18 11:58:41 +0600

    type alias

---
+++ Typescript/introduction/dist/type_Aliass.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let user1 = {
+    name: "Emon",
+    age: 26,
+    hobbies: ['helo', true, 2]
+};
+// console.log(user1);
+function displayInfo(user) {
+    return user;
+}
+let worker = {
+    name: "Emon",
+    salary: 30000
+};
+console.log(worker);
