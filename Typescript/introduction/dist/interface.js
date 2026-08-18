55e6377fbe2cc3a1337b413c7cd0178e4daf569d
fayjullah haque <fayjullah.h.emon@gmail.com>
2026-08-18 17:10:05 +0600

    interface

--- Typescript/introduction/dist/interface.js
+++ Typescript/introduction/dist/interface.js
@@ -1,2 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+let employee = {
+    id: 101,
+    userName: "Fayjullah",
+    salary: 30500
+};
+let employee2 = {
+    id: "201",
+    userName: "Emon",
+};
