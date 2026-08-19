507e627ac04bbec89bf8e7dd4f255e31ba458634
fayjullah haque <fayjullah.h.emon@gmail.com>
2026-08-19 14:12:24 +0600

    literal type

---
+++ Typescript/introduction/dist/literal_types.js
@@ -0,0 +1,11 @@
+"use strict";
+//jokhon kono variable ke custom type dite chai tokhn shei type ke literal type bole
+Object.defineProperty(exports, "__esModule", { value: true });
+let roleType = "Moderator";
+let value = 5;
+// let isVarified:IsVarified = false;//error 
+let isVarified = true;
+const setTheme = (theme) => {
+    console.log(theme);
+};
+setTheme("dark");
