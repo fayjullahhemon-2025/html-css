ef3170c70da95dcdd3f83320cd609fef96bb0794
fayjullah haque <fayjullah.h.emon@gmail.com>
2026-08-20 23:29:44 +0600

    problem-7

---
+++ Typescript/introduction/dist/Problem-7.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function getDistanceSummary(distances) {
+    const total = distances.reduce((acc, curVal) => acc + curVal, 0);
+    let avg;
+    if (distances.length > 0) {
+        avg = total / distances.length;
+    }
+    else {
+        avg = 0;
+    }
+    return { total, average: Number.isNaN(avg) ? 0 : avg };
+}
+console.log(getDistanceSummary([4, 6, 5, 9]));
+console.log(getDistanceSummary([10, 10]));
+console.log(getDistanceSummary([]));
