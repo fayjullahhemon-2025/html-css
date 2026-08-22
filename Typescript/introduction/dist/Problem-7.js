0b733ecf8e01f542492ee442409a20c4f537c98a
fayjullah haque <fayjullah.h.emon@gmail.com>
2026-08-22 19:52:26 +0600

    I don't want to break streak2

--- Typescript/introduction/dist/Problem-7.js
+++ Typescript/introduction/dist/Problem-7.js
@@ -16,3 +16,4 @@ console.log(getDistanceSummary([10, 10]));
 console.log(getDistanceSummary([]));
 console.log(getDistanceSummary([3, 2, 3, 4]));
 console.log(getDistanceSummary([3, 2, 3, 0]));
+console.log(getDistanceSummary([3, 2, 3, 1]));
