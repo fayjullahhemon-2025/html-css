40f14e71b6e7f7e2b395a5bae910e53fe762c9b8
fayjullah haque <fayjullah.h.emon@gmail.com>
2026-08-24 22:47:03 +0600

    avoid streak break

---
+++ Typescript/introduction/dist/practice1.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Directions;
+(function (Directions) {
+    Directions["Up"] = "Up";
+    Directions["Down"] = "Down";
+    Directions["Left"] = "Left";
+    Directions["Right"] = "Right";
+})(Directions || (Directions = {}));
+console.log(Directions.Down);
