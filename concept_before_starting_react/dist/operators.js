62716c92d89679fa77bd0d63311d987aad0b29af
fayjullah haque <fayjullah.h.emon@gmail.com>
2026-08-27 21:41:36 +0600

    operator &&

---
+++ concept_before_starting_react/dist/operators.js
@@ -0,0 +1,21 @@
+"use strict";
+//logical && operator
+// let age = 17;
+// let hasID = true;
+// if(age>=18 && hasID){
+//     console.log("You can enter");
+// }else{
+//     console.log("vag")
+// }
+Object.defineProperty(exports, "__esModule", { value: true });
+const userName = null; //truthy
+if (userName) {
+    console.log(userName);
+}
+else {
+    console.log("Loading....");
+}
+//left=true     right=true , return username
+const result = userName && "User name is " + userName; //short circuit
+console.log(result);
+//# sourceMappingURL=operators.js.map
