8ac07fb793c80700c333e7aba08544bbd7475ffd
fayjullah haque <fayjullah.h.emon@gmail.com>
2026-08-27 22:07:08 +0600

    optional chaining 2

--- concept_before_starting_react/dist/optional_chaining.js
+++ concept_before_starting_react/dist/optional_chaining.js
@@ -5,5 +5,18 @@ const user = {
     name: "Nowshin",
     // city:"Nakla"
 };
-console.log(user?.city);
+// console.log(user?.city);
+//nullish coalescing
+// const name = undefined;
+const name = "Emon";
+//name thakle dekhabo na thakle loading.
+//jdi variable e null or undefined store thake  tobe kebl ami nullish coalescing use korbo
+// if(name){
+//     console.log(name);
+// }else{
+//     console.log("...loading");
+// }
+// name?console.log(name):console.log("loading");
+// name ?? console.log("Loading...")
+console.log(name ?? "Loading...");
 //# sourceMappingURL=optional_chaining.js.map
