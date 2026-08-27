6273e67f9fb3bc06dcc66cd3275b6e7a87a207d0
fayjullah haque <fayjullah.h.emon@gmail.com>
2026-08-27 21:13:56 +0600

    methods.ts

---
+++ concept_before_starting_react/dist/methods.js
@@ -0,0 +1,23 @@
+"use strict";
+//filter(),map(),find(),includes()
+//filter() ===>delete,insert
+//map()===> showingg data on the UI. shobcheye beshi use hoy map react e 
+//find()===>find out specific data
+//includes()===> array or string e kno element ache kina
+Object.defineProperty(exports, "__esModule", { value: true });
+const numbers = [1, 2, 3, 4, 5, 6];
+// const newArr = numbers.filter(num=> num!==2);
+const newArr = numbers.map((num, index) => {
+    // console.log(index)
+    // return num
+});
+// console.log(newArr);
+const items = ['book', 'laptop', 'tablet'];
+const showData = items.map((item, index) => {
+    console.log(`${index + 1}. ${item}`);
+});
+// console.log(showData)
+const products = ["Book", "Pen", "Laptop"];
+const searchProduct = products.find(item => item === 'Pen');
+// console.log(searchProduct)
+//# sourceMappingURL=methods.js.map
