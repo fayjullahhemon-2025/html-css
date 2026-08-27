e0a60909e1294bc17a693179b7be360ba4b2ec24
fayjullah haque <fayjullah.h.emon@gmail.com>
2026-08-27 20:28:56 +0600

    dest.ts 2

--- concept_before_starting_react/dist/dest.js
+++ concept_before_starting_react/dist/dest.js
@@ -10,8 +10,17 @@ const name = user.name;
 // const age = user.age;
 // const city = user.city;
 const { name: userName, age, city } = user; //destructuring
-console.log(userName);
-console.log(age);
-console.log(city);
-console.log(name);
+// console.log(userName);
+// console.log(age);
+// console.log(city);
+// console.log(name);
+const arr = [10, 20, 30];
+// const first = arr[0];
+// const second = arr[1];
+const [first, second] = arr;
+function num() {
+    return [1, 2, 3, 4];
+}
+const [one, two] = num();
+console.log(one, two);
 //# sourceMappingURL=dest.js.map
