a43cbcaf5da76ce0a1c8a92235ba9ec4ba22a079
fayjullah haque <fayjullah.h.emon@gmail.com>
2026-08-27 20:39:18 +0600

    spread operator in obj

--- concept_before_starting_react/dist/spread_operator.js
+++ concept_before_starting_react/dist/spread_operator.js
@@ -6,5 +6,15 @@ const arr = [1, 2, 3, 4];
 const newArr = [...arr]; //shallow copy
 newArr.push(5);
 newArr.push(6);
-console.log(arr);
+// console.log(arr);
+const user = {
+    name: "Emon",
+    age: 26,
+    city: "Dhaka"
+};
+// const newUser = user;
+const newUser = { ...user, image: "url---" }; //spread operator
+// newUser.image = 'url---';
+console.log(newUser);
+console.log("Original obj", user);
 //# sourceMappingURL=spread_operator.js.map
