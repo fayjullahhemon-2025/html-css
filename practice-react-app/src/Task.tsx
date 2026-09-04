6ed43addc3686ad7065fbc2477a82e8bb6969aea
fayjullah haque <fayjullah.h.emon@gmail.com>
2026-09-04 15:23:44 +0600

    rendering data using short circuite

--- practice-react-app/src/Task.tsx
+++ practice-react-app/src/Task.tsx
@@ -6,11 +6,27 @@ interface TaskPropTypes{
 }
 
 export default function Task({name,isDone}:TaskPropTypes){
+    // return isDone === true && <li>Completed: {name}</li>
+    // return isDone === false && <li>Pending: {name}</li>
+    // return isDone === true || <li>Pending: {name}</li> 
+    // return isDone === false || <li>Completed: {name}</li>
+    let list;
     if(isDone){
-        return <li>Completed: {name}</li>
+        list = <li>Completed: {name}</li>
     }
-    // else{
-    //     return <li>Pending: {name}</li>
-    // }
-    return <li>Pending: {name}</li>
+    else{
+        list = <li>Pending: {name}</li>
+    }
+    return list;
 }
+
+//normal if else
+// export default function Task({name,isDone}:TaskPropTypes){
+//     if(isDone){
+//         return <li>Completed: {name}</li>
+//     }
+//     // else{
+//     //     return <li>Pending: {name}</li>
+//     // }
+//     return <li>Pending: {name}</li>
+// }
